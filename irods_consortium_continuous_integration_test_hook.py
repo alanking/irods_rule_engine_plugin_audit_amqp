af84c4eca350a76ce2070fbb5a2502db592ad4de
Alan King <alanking@renci.org>
2020-01-06 11:58:10 -0500

    Use qpid-proton external

--- irods_consortium_continuous_integration_test_hook.py
+++ irods_consortium_continuous_integration_test_hook.py
@@ -8,76 +8,6 @@ import time
 import tempfile
 import irods_python_ci_utilities
 
-
-def get_build_prerequisites_all():
-    return[]
-
-
-def get_build_prerequisites_apt():
-    if irods_python_ci_utilities.get_distribution_version_major() == '12':
-        return['openjdk-7-jre']+get_build_prerequisites_all()
-
-    else:
-        return['default-jre']+get_build_prerequisites_all()
-
-
-def get_build_prerequisites_yum():
-    return['java-1.7.0-openjdk-devel']+get_build_prerequisites_all()
-
-
-def get_build_prerequisites_zypper():
-    return['java-1.7.0-openjdk-devel']+get_build_prerequisites_all()
-
-
-def get_build_prerequisites():
-    dispatch_map = {
-        'Ubuntu': get_build_prerequisites_apt,
-        'Centos': get_build_prerequisites_yum,
-        'Centos linux': get_build_prerequisites_yum,
-        'Opensuse': get_build_prerequisites_zypper,
-    }
-    try:
-        return dispatch_map[irods_python_ci_utilities.get_distribution()]()
-    except KeyError:
-        irods_python_ci_utilities.raise_not_implemented_for_distribution()
-
-
-def install_build_prerequisites():
-    irods_python_ci_utilities.subprocess_get_output(['sudo', '-EH', 'pip', 'install', 'unittest-xml-reporting==1.14.0'])
-
-    if irods_python_ci_utilities.get_distribution() == 'Ubuntu': # cmake from externals requires newer libstdc++ on ub12
-        if irods_python_ci_utilities.get_distribution_version_major() == '12':
-            irods_python_ci_utilities.install_os_packages(['python-software-properties'])
-            irods_python_ci_utilities.subprocess_get_output(['sudo', 'add-apt-repository', '-y', 'ppa:ubuntu-toolchain-r/test'], check_rc=True)
-            irods_python_ci_utilities.subprocess_get_output(['sudo', 'update-java-alternatives', '--set', 'java-1.7.0-openjdk-amd64'])
-            irods_python_ci_utilities.install_os_packages(['libstdc++6'])
-
-    irods_python_ci_utilities.install_os_packages(get_build_prerequisites())
-
-
-def install_qpid_proton():
-    local_qpid_proton_dir = tempfile.mkdtemp(prefix='qpid_proton_dir')
-    irods_python_ci_utilities.git_clone('https://github.com/apache/qpid-proton.git', '0.17.0', local_qpid_proton_dir)
-
-    if irods_python_ci_utilities.get_distribution() == 'Ubuntu':
-        irods_python_ci_utilities.subprocess_get_output(['sudo', 'apt-get', 'install', 'gcc', 'cmake', 'cmake-curses-gui', 'uuid-dev', '-y'])
-        irods_python_ci_utilities.subprocess_get_output(['sudo', 'apt-get', 'install', 'libssl-dev', '-y'])
-        irods_python_ci_utilities.subprocess_get_output(['sudo', 'apt-get', 'install', 'libsasl2-2','libsasl2-dev', '-y'])
-        irods_python_ci_utilities.subprocess_get_output(['sudo', 'apt-get', 'install', 'swig', 'python-dev', 'ruby-dev', 'libperl-dev', '-y'])
-    else:
-        irods_python_ci_utilities.subprocess_get_output(['sudo', 'yum', 'install', 'gcc', 'make', 'cmake', 'libuuid-devel', '-y'])
-        irods_python_ci_utilities.subprocess_get_output(['sudo', 'yum', 'install', 'openssl-devel', '-y'])
-        irods_python_ci_utilities.subprocess_get_output(['sudo', 'yum', 'install', 'cyrus-sasl-devel', '-y'])
-        irods_python_ci_utilities.subprocess_get_output(['sudo', 'yum', 'install', 'swig', 'python-devel', 'ruby-devel', 'rubygem-minitest', 'php-devel', 'perl-devel', '-y'])
-
-    qpid_proton_build_dir = local_qpid_proton_dir + '/build'
-    if not os.path.exists(qpid_proton_build_dir):
-        os.makedirs(qpid_proton_build_dir)
-        os.chdir(qpid_proton_build_dir)
-        irods_python_ci_utilities.subprocess_get_output(['sudo', 'cmake', '..', '-DCMAKE_INSTALL_PREFIX=/usr', '-DSYSINSTALL_BINDINGS=ON'])
-        irods_python_ci_utilities.subprocess_get_output(['sudo', 'make', 'install'])
-
-
 def install_messaging_package(message_broker):
     if 'apache-activemq-' in message_broker:
         version_number = message_broker.split('-')[2]
@@ -128,8 +58,8 @@ def main():
     irods_python_ci_utilities.install_os_packages_from_files(glob.glob(os.path.join(os_specific_directory, 'irods-rule-engine-plugin-audit-amqp*.{0}'.format(package_suffix))))
     irods_python_ci_utilities.subprocess_get_output(['sudo', 'su', '-', 'irods', '-c', 'python2 scripts/add_audit_rule_engine_to_rule_engines.py'], check_rc=True)
 
-    install_build_prerequisites()
-    install_qpid_proton()
+    irods_python_ci_utilities.subprocess_get_output(['sudo', '-EH', 'pip', 'install', 'unittest-xml-reporting==1.14.0'])
+    irods_python_ci_utilities.install_os_packages(['irods-externals-qpid-with-proton0.34-0'])
     install_messaging_package(options.message_broker)
 
     time.sleep(10)
