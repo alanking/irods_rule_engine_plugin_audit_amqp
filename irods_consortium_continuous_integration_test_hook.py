c0ec2b551b231c6d64edda9e483e51c23370801e
Alan King <alanking@renci.org>
2020-01-07 17:58:49 -0500

    Cleanup CMakeLists a bit

--- irods_consortium_continuous_integration_test_hook.py
+++ irods_consortium_continuous_integration_test_hook.py
@@ -43,8 +43,6 @@ def get_build_prerequisites():
 
 
 def install_build_prerequisites():
-    irods_python_ci_utilities.subprocess_get_output(['sudo', '-EH', 'pip', 'install', 'unittest-xml-reporting==1.14.0'])
-
     if irods_python_ci_utilities.get_distribution() == 'Ubuntu': # cmake from externals requires newer libstdc++ on ub12
         if irods_python_ci_utilities.get_distribution_version_major() == '12':
             irods_python_ci_utilities.install_os_packages(['python-software-properties'])
